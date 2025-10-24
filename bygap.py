9ab0deafedc033299de3dd3e1b3645d307a8d41e
Max Nordberg <maxnordberg@macbook-air-von-max.local>
2025-10-24 13:29:38 +0200

    Initial commit

---
+++ bygap.py
@@ -0,0 +1,71 @@
+import csv
+
+# Add 2025 to Super Licence system
+
+with open("data.csv") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+    # VARIABLES
+    elo_br: int  # Elo before round()
+    elo: int  # Elo after round()
+    pole_mult: int = 3  # Pole Multiplicator = 3
+    pod_mult: int = 5  # Podium Multiplicator = 5
+    win_mult: int = 10  # Win Multiplicator = 10
+    pole_sc: int  # Final Pole Elo Score
+    pod_sc: int  # Final Podium Elo Score
+    win_sc: int  # Final Win Elo Score
+    driver_elo: dict = {}  # List to store elo data for each driver
+    season_mult: float
+    element: int  # Used in for loop later
+    count: int = 1  # Used in for loop later
+    gap2_mult : float = 1.2 # Gap to 2nd placed driver gets a boost
+    gap3_mult : float = 1 # Gap to 3rd placed driver stays the same
+
+    for row in reader: # Assign variables with their values
+        driver = (row[1])
+        win_amnt = float(row[2])
+        pod_amnt = float(row[3])
+        pole_amnt = float(row[4])
+        race_amnt = float(row[5])
+        season = int(row[7])
+        gap2 = int(row[8])
+        gap3 = int(row[9])
+
+        # Calculate multiplicator for winning in season X of a single seater career
+        if (season == 3):
+            season_mult = 1.0
+        elif (season == 4):
+            season_mult = 0.8
+        elif (season == 5):
+            season_mult = 0.6
+        elif (season == 1):
+            season_mult = 1.2
+
+        # Final scores
+        pole_sc = (pole_amnt * pole_mult) * season_mult
+        pod_sc = (pod_amnt * pod_mult) * season_mult
+        win_sc = (win_amnt * win_mult) * season_mult
+        gap2_sc = (gap2 / race_amnt) * gap2_mult
+        gap3_sc = (gap3 / race_amnt) * gap3_mult
+
+        # Calculate Elo before rounding, then round it
+        elo_br = float((win_sc + pod_sc + pole_sc) / race_amnt) * (gap2_sc+gap3_sc)
+        elo_ar = round(elo_br, 2)
+
+        if driver is not driver_elo:
+            driver_elo[driver] = []
+        driver_elo[driver] = elo_ar
+
+    # Sort dictionary
+    sorted_elo = sorted(driver_elo.items(), key=lambda item: item[1], reverse=True)
+
+    # Print results
+    print()
+    print("Ranked by Gap")
+    print("Pos.", "Name", " ", " ", " ", " ", " ", "|", "Elo")
+    for element in sorted_elo:
+        print(count, ".", element[0], "|", element[1])
+        count += 1
+
+
---
+++ bysof.py
@@ -0,0 +1,64 @@
+import csv
+
+# Add 2025 to Super Licence system
+
+with open("data.csv") as file:
+    reader = csv.reader(file)
+    next(reader)
+
+# VARIABLES
+    elo_br : int # Elo before round()
+    elo : int # Elo after round()
+    pole_mult : int = 3 # Pole Multiplicator = 3
+    pod_mult : int = 5 # Podium Multiplicator = 5
+    win_mult : int = 10 # Win Multiplicator = 10
+    pole_sc : int # Final Pole Elo Score
+    pod_sc: int # Final Podium Elo Score
+    win_sc: int # Final Win Elo Score
+    driver_elo : dict = {} # List to store elo data for each driver
+    season_mult : float
+    element : int # Used in for loop later
+    count : int = 1 # Used in for loop later
+
+    for row in reader:
+        driver = (row[1])
+        win_amnt = float(row[2])
+        pod_amnt = float(row[3])
+        pole_amnt = float(row[4])
+        race_amnt = float(row[5])
+        sof = float(row[6])
+        season = int(row[7])
+
+        # Calculate multiplicator for winning in season X of a single seater career
+        if (season == 3):
+            season_mult = 1.0
+        elif (season == 4):
+            season_mult = 0.8
+        elif (season == 5):
+            season_mult = 0.6
+        elif (season ==1):
+            season_mult = 1.2
+
+        pole_sc = (pole_amnt * pole_mult)*season_mult
+        pod_sc = (pod_amnt * pod_mult)*season_mult
+        win_sc = (win_amnt * win_mult)*season_mult
+
+        elo_br = float((win_sc + pod_sc + pole_sc)/race_amnt)*sof
+        elo = round(elo_br, 2)
+
+        if driver is not driver_elo:
+            driver_elo[driver] = []
+        driver_elo[driver] = elo
+
+
+    sorted_elo = sorted(driver_elo.items(), key=lambda item: item[1], reverse = True)
+
+    print()
+    print("Ranked by Elo")
+    print("Pos.", "Name", " ", " "," ", " "," ", "|", "Elo")
+
+    for element in sorted_elo:
+        print(count, ".", element[0], "|", element[1])
+        count += 1
+    print()
+
